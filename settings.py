2615d481de42c95a251ec02bc9448e5c44278d91
mnicnc404 <mnicnc404@gmail.com>
2017-10-15 15:40:56 +0800

    trained with 400 epochs

--- settings.py
+++ settings.py
@@ -3,18 +3,18 @@ PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
 
 # trained model
-META_PATH = '/'.join((PROJECT_PATH, 'models/model.ckpt.meta'))
-SAVE_PATH = '/'.join((PROJECT_PATH, 'models/model.ckpt'))
+META_PATH = os.path.join(PROJECT_PATH, 'models', 'model.ckpt.meta')
+SAVE_PATH = os.path.join(PROJECT_PATH, 'models', 'model.ckpt')
 
 # frozen model
-PB_PATH = '/'.join((PROJECT_PATH, 'models/frozen.pb'))
+PB_PATH = os.path.join(PROJECT_PATH, 'models', 'frozen.pb')
 
 
 # model meta
-TRAIN_DIR = '/'.join((PROJECT_PATH, 'datasets/train/'))
-TEST_DIR = '/'.join((PROJECT_PATH, 'datasets/test1'))
+TRAIN_DIR = os.path.join(PROJECT_PATH, 'datasets', 'train')
+TEST_DIR = os.path.join(PROJECT_PATH, 'datasets', 'test1')
 TRAIN_X_MEAN_NPY = os.path.join(TRAIN_DIR, 'train_x_mean.npy')
 TRAIN_X_STD_NPY = os.path.join(TRAIN_DIR, 'train_x_std.npy')
 
 # Log for tensorboard
-LOG_DIR = '/tmp/tensorboard/cat-recognizer/'
+LOG_DIR = os.path.join('tmp', 'tensorboard', 'cat-recognizer')
