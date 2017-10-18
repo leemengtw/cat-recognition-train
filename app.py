3d2aa6e2c120d72ca592a098ef96fe38cb978322
leemeng <b98705001@gmail.com>
2017-10-18 17:43:25 +0900

    Deploy version of Flask app
    - UI refinement
    - AWS saving info

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import os
+import boto3
+import json
 from flask import Flask, flash, request, json,\
         render_template, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from flask_bootstrap import Bootstrap
 from predict import Predictor
+from settings import UPLOAD_FOLDER, ALLOWED_EXTENSIONS, IMAGE_INFO_JSON, IS_DEBUG
 
-BUCKET_NAME = 'machine-learning-models-dev'
-MODEL_FILE_NAME = 'purchase-boost/random_forest_v1.pkl'
-MODEL_LOCAL_PATH = '/tmp/' + MODEL_FILE_NAME.split('/')[-1]
+# AWS settings, ignore this part if test locally
+try:
+    from aws_settings import S3_BUCKET_NAME, S3_BUCKET_BASE_URL,\
+        AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY
+    SAVE_INFO_ON_AWS = True
+except ImportError:
+    SAVE_INFO_ON_AWS = False
 
-UPLOAD_FOLDER = 'static/uploaded_images'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -20,9 +25,18 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 predictor = Predictor()
 
+# used for rendering after feedback
+CUR_PROB = None
+CUR_FILENAME = None
+
 
 @app.route('/', methods=['GET', 'POST'])
 def make_prediction():
+    """View that receive images and render predictions
+    """
+    global CUR_PROB
+    global CUR_FILENAME
+
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
@@ -35,24 +49,77 @@ def make_prediction():
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # security concerns
+            # security concerns
+            filename = secure_filename(file.filename)
 
-            # save image locally
-            if not os.path.exists(app.config['UPLOAD_FOLDER']):
-                os.makedirs(app.config['UPLOAD_FOLDER'])
-
-            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(file_path)
+            # save image for prediction and rendering
+            file_path = save_image(file, filename)
 
             # get prediction
             prob = predictor.predict(file_path)
 
+            # save image info
+            save_image_info(filename, prob)
+
+
+            # keep record of current prediction for later rendering
+            # after getting user feedback
+            CUR_PROB, CUR_FILENAME = prob, filename
+
+            # get information of gallery
+            images, cur_accuracy, num_stored_images = get_stat_of_recent_images()
+
             return render_template(
                     'index.html',
-                    prob=float('{:.1f}'.format(prob * 100)),
-                    cur_image_path=uploaded_image_path(filename))
+                    cat_prob=float('{:.1f}'.format(prob * 100)),
+                    dog_prob=float('{:.1f}'.format((1 - prob) * 100)),
+                    cur_image_path=file_path,
+                    images=images,
+                    num_stored_images=num_stored_images,
+                    cur_accuracy=cur_accuracy,
+                    show_feedback=True)
 
-    return render_template('index.html')
+    # get information of gallery when receive GET request
+    images, cur_accuracy, num_stored_images = get_stat_of_recent_images()
+    return render_template(
+            'index.html', images=images, cur_accuracy=cur_accuracy,
+            num_stored_images=num_stored_images)
+
+
+@app.route('/feedback', methods=['POST'])
+def save_user_feedback():
+    """Save user feedback of current prediction"""
+    label = request.form['label']
+
+    # save user feedback in file
+    with open(IMAGE_INFO_JSON, 'r') as f:
+        image_info = json.load(f)
+        image_info[CUR_FILENAME]['label'] = label
+    with open(IMAGE_INFO_JSON, 'w') as f:
+        json.dump(image_info, f, indent=4)
+
+    if SAVE_INFO_ON_AWS:
+        save_image_info_on_s3(image_info)
+
+    # get information of gallery
+    images, cur_accuracy, num_stored_images = get_stat_of_recent_images()
+
+
+    return render_template(
+            'index.html',
+            prob=float('{:.1f}'.format(CUR_PROB * 100)),
+            cur_image_path=uploaded_image_path(CUR_FILENAME),
+            images=images,
+            num_stored_images=num_stored_images,
+            cur_accuracy=cur_accuracy,
+            show_thankyou=True)
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    """generate url for user uploaded file"""
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 def allowed_file(filename):
@@ -66,88 +133,176 @@ def uploaded_image_path(filename):
     return '/'.join((app.config['UPLOAD_FOLDER'], filename))
 
 
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    """generate url for user uploaded file"""
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+def get_stat_of_recent_images(num_images=300):
+    """Return information of recent uploaded images for galley rendering
 
+    Parameters
+    ----------
+    num_images: int
+        number of images to show at once
+    Returns
+    -------
+    image_stats: list of dicts representing images in last modified order
+        path: str
+        label: str
+        pred: str
+        cat_prob: int
+        dog_prob: int
 
-@app.route('/image-test', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)  # security concerns
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <p><input type=file name=file>
-         <input type=submit value=Upload>
-    </form>
-    '''
+    cur_accuracy: float
+    num_stored_images: int
+        indepenent of num_images param, the total number of images available
 
-
-@app.route('/test', methods=['POST'])
-def index():
-    payload = json.loads(request.get_data().decode('utf-8'))
-    prediction = predict(payload['data'])
-    return json.dumps(prediction)
-
-
-def load_model():
-    """Load model stored in S3 for making prediction.
-    Make sure to update newest model for correct predictions.
-    If model is downloaded in previous execution, use existing model.
-
-    Return:
-    model object
     """
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket(BUCKET_NAME)
-
-    if not os.path.isfile(MODEL_LOCAL_PATH):
-        bucket.download_file(MODEL_FILE_NAME, MODEL_LOCAL_PATH)
-
-    return joblib.load(MODEL_LOCAL_PATH)
+    folder = app.config['UPLOAD_FOLDER']
 
 
-def predict(data):
-    """Use loaded model for making prediction and reture the result.
+    # get list of last modified images
+    files = ['/'.join((folder, file)) \
+        for file in os.listdir(folder) if 'json' not in file] # exclude .json file
 
-    Parameters:
-    data: List of test instances with all ordered feature values
-        [[1, 1, 0, ...]]
+    # list of tuples (file_path, timestamp)
+    last_modified_files = [(file, os.path.getmtime(file)) for file in files]
+    last_modified_files = sorted(last_modified_files,
+                            key=lambda t: t[1], reverse=True)
+    num_stored_images = len(last_modified_files) - 1
 
-    Return:
-        result: Dict
-        {
-            'purchase_probability': 0.46,
-            'purchasable': 0
+
+    # read in image info
+    with open(IMAGE_INFO_JSON, 'r') as f:
+        info = json.load(f)
+
+    # build info for images
+    image_stats = []
+    for i, f in enumerate(last_modified_files):
+        # set limit for rendering pictures
+        if i > num_images: break
+
+        path, filename = f[0], f[0].replace(folder, '').replace('/', '')
+        cur_image_info = info.get(filename, {})
+
+        # ignore images don't have info for now
+        if not cur_image_info: continue
+
+        prob = cur_image_info['prob']
+        image = {
+            'path': path,
+            'label': cur_image_info.get('label', 'unknown'),
+            'pred': cur_image_info['pred'],
+            'cat_prob': int(prob * 100),
+            'dog_prob': int((1 - prob) * 100),
         }
-    """
-    model = load_model()
-    prob = model.predict_proba(data)
-    result = {
-        'purchase_probability': float(prob[0][1]),
-        'purchasable': int(model.predict(data)[0])
-    }
+        image_stats.append(image)
 
-    return result
+    # comput current accuracy if labels available
+    total, correct = 0, 0
+    for image in image_stats:
+        if image['label'] != 'unknown':
+            total += 1
+            if image['label'] == image['pred']:
+                correct += 1
+
+    try:
+        cur_accuracy = float('{:.1f}'.format(correct / float(total)))
+    except ZeroDivisionError:
+        cur_accuracy = 0
+
+    # print(image_stats)
+    # print(cur_accuracy)
+
+    return image_stats, cur_accuracy, num_stored_images
+
+
+def save_image(file, filename):
+    """Save current images to setting.UPLOAD_FOLDER and return the
+    corresponding file path. Also save the image to AWS S3 if aws information
+    is available.
+
+    Parameters
+    ----------
+    file: werkzeug.datastructures.FileStorage
+    filename: str
+        pure file name with extension
+
+    Returns
+    -------
+    file_path: str
+        the complete path of the saved image
+
+    """
+
+    # create folder for storing images if not exist
+    if not os.path.exists(app.config['UPLOAD_FOLDER']):
+        os.makedirs(app.config['UPLOAD_FOLDER'])
+
+    # save image locally
+    file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(file_path)
+
+    # save image to S3
+    if SAVE_INFO_ON_AWS:
+        client = boto3.client('s3',
+            aws_access_key_id=AWS_ACCESS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        )
+
+        file.seek(0)
+        client.put_object(
+            Body=file.read(),
+            Bucket=S3_BUCKET_NAME,
+            Key=filename)
+
+    return file_path
+
+
+def save_image_info(filename, prob):
+    """Save predicted result of the image in a json file locally.
+    Also save the json to AWS S3 if aws information is available.
+
+    Parameters
+    ----------
+    filename: str
+        pure file name with extension
+    prob: float
+        the probability of the image being cat
+    """
+    # save prediction info locally
+    with open(IMAGE_INFO_JSON, 'r') as f:
+        image_info = json.load(f)
+
+        if image_info.get(filename, None):
+            pass
+        else:
+            image_info[filename] = {
+                'prob': float(prob),
+                'y_pred': 1 if prob > 0.5 else 0,
+                'pred': 'cat' if prob > 0.5 else 'dog'
+            }
+    with open(IMAGE_INFO_JSON, 'w') as f:
+        json.dump(image_info, f, indent=4)
+
+    # save image info to S3
+    if SAVE_INFO_ON_AWS:
+        save_image_info_on_s3(image_info)
+
+
+def save_image_info_on_s3(image_info):
+    """Save the json file containing image info on S3
+
+    Parameters
+    ----------
+    image_info: dict
+    """
+    client = boto3.client('s3',
+        aws_access_key_id=AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+    )
+    client.put_object(
+        Body=json.dumps(image_info, indent=4),
+        Bucket=S3_BUCKET_NAME,
+        Key=IMAGE_INFO_JSON\
+        .replace(app.config['UPLOAD_FOLDER'], '').replace('/', ''))
+
 
 
 @app.errorhandler(404)
@@ -158,4 +313,4 @@ def page_not_found(e):
 if __name__ == '__main__':
     pass
     # application.run(host='0.0.0.0', port=5602)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=IS_DEBUG)
--- settings.py
+++ settings.py
@@ -1,6 +1,9 @@
 import os
 PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 
+# deploy application
+IS_DEBUG = True
+
 
 # trained model
 META_PATH = os.path.join(PROJECT_PATH, 'models', 'model.ckpt.meta')
@@ -17,4 +20,9 @@ TRAIN_X_MEAN_NPY = os.path.join(TRAIN_DIR, 'train_x_mean.npy')
 TRAIN_X_STD_NPY = os.path.join(TRAIN_DIR, 'train_x_std.npy')
 
 # Log for tensorboard
-LOG_DIR = os.path.join('tmp', 'tensorboard', 'cat-recognizer')
+LOG_DIR = os.path.join(PROJECT_PATH, 'tensorboard', 'logs')
+
+# image-related
+UPLOAD_FOLDER = 'static/uploaded_images'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+IMAGE_INFO_JSON = os.path.join(UPLOAD_FOLDER, 'image_info.json')
